024bc2336b1c794f24e6fd16c9ddecb4d84b6fc9
Sergey Ushakov <sergey.ushakov94@gmail.com>
2020-11-06 09:14:48 +0200

    added a 404 route

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
 import Router from "./router/Router.js";
 import Route from "./router/Route.js";
 
-const router = new Router(document.getElementById("root"), [
+new Router(document.getElementById("root"), [
   new Route("/pages/kek", "/views/test/test.html"),
   new Route("/pages/lol", "/views/route/index.html"),
+  new Route(null, "/views/404/index.html"),
 ]);
