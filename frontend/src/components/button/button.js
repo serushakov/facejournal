5a817dceeeea7f089679eaf6ba25b1b0225828d8
Sergey Ushakov <sergey.ushakov94@gmail.com>
2020-11-08 11:38:24 +0200

    added form submitting functionality to button

--- frontend/src/components/button/button.js
+++ frontend/src/components/button/button.js
@@ -5,6 +5,8 @@ class Button extends HTMLElement {
     const shadowRoot = this.attachShadow({
       mode: "open",
     }).appendChild(template.content.cloneNode(true));
+
+    this.loadStyle();
   }
 
   connectedCallback() {
@@ -12,9 +14,61 @@ class Button extends HTMLElement {
       throw Error("Button should have a variant attribute");
     }
 
-    const buttonElement = this.shadowRoot.getElementById("button");
+    this.buttonElement = this.shadowRoot.getElementById("button");
 
-    buttonElement.classList.add(this.attributes.variant?.value);
+    for (let attribute of Array.from(this.attributes)) {
+      this.setButtonAttribute(attribute.name, attribute.value);
+    }
+
+    this.addEventListener("click", this.handleClick);
+  }
+
+  handleClick = () => {
+    const formSelector = this.attributes.getNamedItem("form-selector");
+    console.log(formSelector);
+    if (formSelector) {
+      const form = document.querySelector(formSelector.value);
+      if (!form) throw Error("No form found with selector " + formSelector);
+
+      // Creating a hidden submit button to submit the form
+      const button = document.createElement("button");
+      button.type = "submit";
+
+      form.appendChild(button);
+      button.click();
+      button.remove();
+    }
+  };
+
+  async loadStyle() {
+    this.shadowRoot.append(
+      await loadStyles("/components/button/button.css"),
+      await loadStyles("/reset.css")
+    );
+  }
+
+  setButtonAttribute(name, value, oldValue) {
+    switch (name) {
+      case "id":
+        break;
+      case "class":
+        this.buttonElement.classList.add(value);
+        break;
+      case "variant":
+        if (oldValue) {
+          this.buttonElement.classList.remove(oldValue);
+        }
+
+        this.buttonElement.classList.add(value);
+        break;
+      default:
+        this.buttonElement.setAttribute(name, value);
+    }
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(name, oldValue, newValue);
+    this.setButtonAttribute(name, newValue, oldValue);
   }
 }
 
