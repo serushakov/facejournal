0ceae6f658ead74283b49db056254c3883ee0b53
Sergey Ushakov <sergey.ushakov94@gmail.com>
2020-11-18 09:21:46 +0200

    implemented logged in state in header with a logout button

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,7 +1,6 @@
 import store from "/state/index.js";
 import Router from "/router/Router.js";
-
-import * as actions from "/state/auth/actions.js";
+import { logout } from "/state/auth/thunks.js";
 
 class Header extends HTMLElement {
   constructor() {
@@ -24,7 +23,13 @@ class Header extends HTMLElement {
   };
 
   init() {
-    store.subscribe(this.listener);
+    this.loggedInStateContainer = this.shadowRoot.querySelector(
+      "#logged-in-state"
+    );
+    this.loggedOutStateContainer = this.shadowRoot.querySelector(
+      "#logged-out-state"
+    );
+    this.usernameContainer = this.shadowRoot.querySelector("#username");
 
     this.shadowRoot.getElementById("login").addEventListener("click", () => {
       Router.navigate("/login");
@@ -32,14 +37,37 @@ class Header extends HTMLElement {
     this.shadowRoot.getElementById("register").addEventListener("click", () => {
       Router.navigate("/register");
     });
+    this.shadowRoot.querySelector("#logout").addEventListener("click", () => {
+      store.dispatch(logout());
+    });
+
+    store.subscribe(this.listener);
   }
 
   disconnectedCallback() {
     store.unsubscribe(this.listener);
   }
 
-  listener = (state) => {
-    console.log(state.auth);
+  listener = ({ auth: { user } }) => {
+    if (user) {
+      this.showLoggedInState(user);
+    } else {
+      this.showLoggedOutState();
+    }
+  };
+
+  showLoggedInState = ({ firstName, lastName }) => {
+    const fullName = `${firstName} ${lastName}`;
+
+    this.loggedInStateContainer.classList.remove("hidden");
+    this.loggedOutStateContainer.classList.add("hidden");
+    this.usernameContainer.textContent = fullName;
+  };
+
+  showLoggedOutState = () => {
+    this.loggedInStateContainer.classList.add("hidden");
+    this.loggedOutStateContainer.classList.remove("hidden");
+    this.usernameContainer.textContent = "";
   };
 
   loadStyle = async () => {
