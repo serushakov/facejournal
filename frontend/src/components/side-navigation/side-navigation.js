899fe8b32984de3fdd7db42881ee6747fee47ab4
Sergey Ushakov <sergey@ushakov.io>
2020-11-29 11:54:57 +0200

    implemented side-navigation

---
+++ frontend/src/components/side-navigation/side-navigation.js
@@ -0,0 +1,63 @@
+import css from './side-navigation.scss';
+import { loadAndParseHtml } from '/loader.js';
+
+class SideNavigation extends HTMLElement {
+  constructor() {
+    super();
+
+    loadAndParseHtml('/components/side-navigation/side-navigation.html').then(
+      this.mountRoot
+    );
+  }
+
+  navigationItems = [
+    {
+      label: 'Feed',
+      url: '/feed',
+    },
+    {
+      label: 'Friends',
+      url: '/friends',
+    },
+    {
+      label: 'Search',
+      url: '/search',
+    },
+  ];
+
+  mountRoot = (document) => {
+    const template = document.getElementById('side-navigation');
+
+    this.appendChild(template.content.cloneNode(true));
+
+    this.navItemTemplate = this.querySelector('#navigation-item');
+    this.navigationRoot = this.querySelector('#navigation-root');
+
+    this.loadStyle();
+    this.init();
+  };
+
+  loadStyle = () => {
+    const style = document.createElement('style');
+    style.textContent = css;
+
+    this.appendChild(style);
+  };
+
+  init = () => {
+    this.navigationItems.forEach(this.createNavigationItem);
+  };
+
+  createNavigationItem = (navItem) => {
+    const element = this.navItemTemplate.content.cloneNode(true);
+
+    const anchorTag = element.querySelector('a');
+
+    anchorTag.href = navItem.url;
+    anchorTag.textContent = navItem.label;
+
+    this.navigationRoot.appendChild(element);
+  };
+}
+
+customElements.define('side-navigation', SideNavigation);
