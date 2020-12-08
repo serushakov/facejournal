03217d2b77488a2d4656d61c81d8d93eb5f1c490
Sergey Ushakov <sergey@ushakov.io>
2020-12-08 14:23:07 +0200

    added validator result to handleDeleteFriend

--- backend/src/routes/users/handleDeleteFriend.js
+++ backend/src/routes/users/handleDeleteFriend.js
@@ -1,8 +1,14 @@
-const { body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 export const deleteFriendValidators = [body('userId').isUUID().exists()];
 
 const handleDeleteFriend = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array());
+  }
+
   const {
     user,
     body: { userId },
