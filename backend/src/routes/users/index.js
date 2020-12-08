2e2f94460ab2386b2d207e65cdfd91a624918ff2
Sergey Ushakov <sergey@ushakov.io>
2020-12-08 12:01:03 +0200

    added new routes to router

--- backend/src/routes/users/index.js
+++ backend/src/routes/users/index.js
@@ -3,15 +3,22 @@ import passport from 'passport';
 import handleMe from './handleMe';
 import handleSearch, { searchValidators } from './handleSearch';
 import handleGetUser, { getUserValidators } from './handleGetUser';
+import { nonBlockingAuth } from '../../utils';
+import handlePostFriend, { postFriendValidators } from './handlePostFriend';
+import handleDeleteFriend, {
+  deleteFriendValidators,
+} from './handleDeleteFriend';
 
 const router = new express.Router();
 
 router.get('/search', searchValidators, handleSearch);
-router.get('/by_id/:id', getUserValidators, handleGetUser);
+router.get('/by_id/:id', nonBlockingAuth, getUserValidators, handleGetUser);
 
 /* AUTHENTICATED ROUTES BELOW */
 router.use(passport.authenticate('jwt'));
 
 router.get('/me', handleMe);
+router.post('/friend', postFriendValidators, handlePostFriend);
+router.delete('/friend', deleteFriendValidators, handleDeleteFriend);
 
 export default router;
