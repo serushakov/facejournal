66311b8bd4740a3a757eeeb1c89ad88638c63237
Sergey Ushakov <sergey@ushakov.io>
2020-12-09 09:54:37 +0200

    improved Like table design

--- backend/src/database/Like.js
+++ backend/src/database/Like.js
@@ -3,11 +3,17 @@ import sequelize from './sequelize';
 import User from './User';
 
 const Like = sequelize.define('Like', {
-  id: {
+  userId: {
     type: DataTypes.UUID,
-    allowNull: false,
+    unique: 'likeId',
     primaryKey: true,
-    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+  },
+  postId: {
+    type: DataTypes.UUID,
+    unique: 'likeId',
+    primaryKey: true,
+    allowNull: false,
   },
 });
 
